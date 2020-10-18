9179cd3e6f745c6f5745102181e033bb5c747406
Mark Hartmann <contact@mark-hartmann.com>
2020-10-18 16:11:15 +0200

    added src/index.js and updates the entry points

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    const release = core.getInput('release');
+    const manifest = core.getInput('deployment-manifest');
+
+    console.log(`Trying to apply ${release}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+} catch (error) {
+    core.setFailed(error.message);
+}
