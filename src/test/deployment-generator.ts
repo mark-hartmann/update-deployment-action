dcf50286302e1ac8664ebef11e73d4f8fe88ed62
Mark Hartmann <contact@mark-hartmann.com>
2020-10-20 21:27:22 +0200

    rewrote the deployment-generator to produce more usable data

--- src/test/deployment-generator.ts
+++ src/test/deployment-generator.ts
@@ -1,82 +1,137 @@
-import {adjectives, animals, Config, names, NumberDictionary, uniqueNamesGenerator} from 'unique-names-generator';
+import {adjectives, animals, names, uniqueNamesGenerator} from 'unique-names-generator';
 import {testDataDirectory} from "./setup";
 import fs from 'fs';
-import yaml, {Document} from 'yaml';
+import yaml from 'yaml';
 
 export interface DeploymentInfo {
-    path: string;
-    author: string;
+    path: string,
     isValid: boolean;
-    imageName: string
-    initialRelease: string;
+    container: Container,
     nextRelease: string;
 }
 
-const customConfig: Config = {
-    dictionaries: [adjectives, names],
-    separator: '',
-    length: 2,
-    style: "lowerCase"
-};
+export interface Container {
+    name: string,
+    author: string,
+    imageName: string,
+    release: string,
+    repository: string,
+}
 
-export const generateDeployment = (): DeploymentInfo => {
-    const author = uniqueNamesGenerator({
-        dictionaries: [adjectives, names],
-        length: 2,
-        style: "lowerCase"
-    });
-    const imageName = uniqueNamesGenerator({
+export interface Deployment {
+    manifest: string,
+    containers: Container[],
+}
+
+function randomIntFromInterval(min: number, max: number): number { // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const generateImageName = () => {
+    return uniqueNamesGenerator({
         dictionaries: [adjectives, animals],
         length: 2,
         style: "lowerCase",
         separator: '-'
     });
-    const releaseConfig: Config = {
+}
+
+const generateReleaseName = () => {
+    return uniqueNamesGenerator({
         dictionaries: [['r'], animals],
         separator: '-',
         style: "lowerCase"
+    });
+}
+
+const generateAuthorName = () => {
+    return uniqueNamesGenerator({
+        dictionaries: [adjectives, names],
+        length: 2,
+        style: "lowerCase"
+    });
+}
+
+const serviceFragment = (): string => {
+    const templatePath = './src/test/templates/service-template.json';
+    return yaml.stringify(JSON.parse(fs.readFileSync(templatePath, 'utf8')));
+};
+
+
+const deploymentFragment = (): Deployment => {
+
+    const templatePath = './src/test/templates/deployment-template.json';
+    const content = JSON.parse(fs.readFileSync(templatePath, 'utf8'));
+    const nContainers = randomIntFromInterval(1, 4);
+
+    const containers: Container[] = [];
+
+    for (let i = 0; i < nContainers; i++) {
+        const image = generateImageName();
+        const author = generateAuthorName();
+        const release = generateReleaseName();
+        const repository = ['', 'ghcr.io/'][Math.round(Math.random())];
+
+        content.spec.template.spec.containers.push({
+            name: image,
+            image: `${repository}${author}/${image}:${release}`
+        });
+
+        containers.push({
+            name: image,
+            author: author,
+            release: release,
+            imageName: image,
+            repository: repository === '' ? 'Docker' : 'Github'
+        });
+    }
+
+    return {
+        manifest: yaml.stringify(content),
+        containers: containers,
     };
-    const initialRelease = uniqueNamesGenerator(releaseConfig);
-    const nextRelease = uniqueNamesGenerator(releaseConfig);
+};
+
+export const generateDeployment = (): DeploymentInfo => {
 
     const documents = [];
     const maxDocuments = [1, 2, 3, 4];
-    const totalDocuments = maxDocuments[Math.floor(Math.random()*maxDocuments.length)];
+    const totalDocuments = maxDocuments[Math.floor(Math.random() * maxDocuments.length)];
 
     let services = 0;
     let deployments = 0;
+    let containerToChange: Container;
+
     for (let i = 0; i < totalDocuments; i++) {
         const create = deployments > 0
             ? 'Service'
             : ['Service', 'Deployment'][Math.round(Math.random())];
 
         if (create === 'Service') {
+            documents.push(serviceFragment());
             services++;
-            const content = JSON.parse(fs.readFileSync('./src/test/templates/service-template.json', 'utf8'));
-            documents.push(yaml.stringify(content));
         }
 
         if (create === 'Deployment') {
+            const deployment: Deployment = deploymentFragment();
+            const n = deployment.containers.length;
+
+            containerToChange = deployment.containers[Math.floor((Math.random() * n))];
+            documents.push(deployment.manifest);
+
             deployments++;
-
-            const content = JSON.parse(fs.readFileSync('./src/test/templates/deployment-template.json', 'utf8'));
-            const containers = content.spec.template.spec.containers || [];
-            containers.forEach((c: any) => {
-                c.image = `${author}/${imageName}:${initialRelease}`;
-            });
-
-            documents.push(yaml.stringify(content));
         }
     }
 
-    fs.writeFileSync(`${testDataDirectory}/${imageName}-deployment.yaml`, yaml.stringify(documents));
+    const deploymentFile = `${testDataDirectory}/${generateImageName()}-deployment.yaml`;
+
+    fs.writeFileSync(deploymentFile, yaml.stringify(documents));
 
     return {
-        author: uniqueNamesGenerator(customConfig),
-        initialRelease: initialRelease,
-        nextRelease: nextRelease,
         isValid: deployments !== 0,
-        imageName: imageName,
-        path: `${testDataDirectory}/${imageName}-deployment.yaml`
-    }
+        nextRelease: generateReleaseName(),
+        // @ts-ignore
+        container: containerToChange,
+        path: deploymentFile
+    };
 }
