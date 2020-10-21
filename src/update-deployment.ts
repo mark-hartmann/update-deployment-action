ccb3657f71361ae3e469780897a569e5fa7880ae
Mark Hartmann <contact@mark-hartmann.com>
2020-10-21 19:30:20 +0200

    changed update-deployment to run synchronously

--- src/update-deployment.ts
+++ src/update-deployment.ts
@@ -1,47 +1,42 @@
-import * as core from '@actions/core';
+import {getInput} from '@actions/core';
 import yaml, {Document} from 'yaml';
 import fs from 'fs';
 import Parsed = Document.Parsed;
 
-const run = async () => {
-    const image = core.getInput('image');
-    const release = core.getInput('release');
-    const manifest = core.getInput('deployment-manifest');
-
-    console.log(`Trying to apply ${release} to ${manifest}!`);
+const run = () => {
+    const image = process.env.IMAGE || getInput('image');
+    const release = process.env.RELEASE || getInput('release');
+    const manifest = process.env.DEPLOYMENT_MANIFEST || getInput('deployment-manifest');
 
     const individuals: any = [];
-    fs.access(manifest, (err) => {
 
-        if (err) {
-            console.error(err)
-            return
-        }
+    try {
+        fs.accessSync(manifest, fs.constants.R_OK);
+    } catch (e) {
+        throw new Error(e.message);
+    }
 
-        const yamlDocuments = yaml.parseAllDocuments(fs.readFileSync(manifest, 'utf8'));
-        yamlDocuments.forEach((document: Parsed) => {
-            const json = document.toJSON();
+    const yamlDocuments = yaml.parseAllDocuments(fs.readFileSync(manifest, 'utf8'));
 
-            // Check if its a deployment before trying to manipulate it
-            if (!json.kind || json.kind !== 'Deployment') {
-                individuals.push(document);
-            }
+    yamlDocuments.forEach((document: Parsed) => {
+        const json = document.toJSON();
 
+        // Check if its a deployment before trying to manipulate it
+        if (json.kind && json.kind === 'Deployment') {
             const containers = json.spec.template.spec.containers || [];
             containers.forEach((c: any) => {
                 c.image = `${image}:${release}`;
             });
 
             individuals.push(yaml.stringify(json));
-        });
+        } else {
+            individuals.push(document);
+        }
     });
-
-
-    console.log(yaml.stringify(individuals));
 };
 
 if (process.env.NODE_ENV !== 'test') {
     run();
 }
 
-export default {run};
+export {run};
