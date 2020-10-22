fcdefd307345b2c028ca3970a72d31b2a743bb94
Mark Hartmann <contact@mark-hartmann.com>
2020-10-22 19:13:16 +0200

    added some tests

--- src/__tests__/extract-container-data.test.ts
+++ src/__tests__/extract-container-data.test.ts
@@ -1,6 +1,77 @@
 import {extractContainerData} from "../extract-container-data";
+import {adjectives, names, uniqueNamesGenerator} from 'unique-names-generator';
 
-it('does stuff', () => {
-    console.log(extractContainerData('mark-hartmann/img-name:latest'));
-    console.log(extractContainerData('github.io/mark-hartmann/img-name'));
+const getUsername = () => {
+    return uniqueNamesGenerator({
+        dictionaries: [adjectives, names],
+        length: 2,
+        style: "lowerCase"
+    });
+};
+
+const getImageName = () => {
+    return uniqueNamesGenerator({
+        dictionaries: [adjectives, names],
+        length: 2,
+        style: "lowerCase"
+    });
+};
+
+it('extracts the username, image and explicit tag', () => {
+    const username = getUsername();
+    const imageName = getImageName();
+    const release = 'v4.34.764';
+
+    const data = `${username}/${imageName}:${release}`;
+    const extracted = extractContainerData(data);
+
+    expect(extracted.repository).not.toBeDefined();
+    expect(extracted.user).toEqual(username);
+    expect(extracted.image).toEqual(imageName);
+    expect(extracted.release).toEqual(release);
 });
+
+it('extracts the username and image but neither repo or release', () => {
+    const username = getUsername();
+    const imageName = getImageName();
+
+    const data = `${username}/${imageName}`;
+    const extracted = extractContainerData(data);
+
+    expect(extracted.repository).not.toBeDefined();
+    expect(extracted.release).not.toBeDefined();
+
+    expect(extracted.image).toEqual(imageName);
+    expect(extracted.user).toEqual(username);
+});
+
+it('extracts the repo, username and image', () => {
+    const username = getUsername();
+    const imageName = getImageName();
+    const repo = 'ghcr.io';
+
+    const data = `${repo}/${username}/${imageName}`;
+    const extracted = extractContainerData(data);
+
+    expect(extracted.release).not.toBeDefined();
+    expect(extracted.repository).toEqual(repo);
+    expect(extracted.image).toEqual(imageName);
+    expect(extracted.user).toEqual(username);
+});
+
+it('extracts the username, image and explicit tag', () => {
+    const username = getUsername();
+    const imageName = getImageName();
+    const release = 'v4.34.764';
+    const repo = 'ghcr.io';
+
+    const data = `${repo}/${username}/${imageName}:${release}`;
+    const extracted = extractContainerData(data);
+
+    expect(extracted.repository).toEqual(repo);
+    expect(extracted.user).toEqual(username);
+    expect(extracted.image).toEqual(imageName);
+    expect(extracted.release).toEqual(release);
+});
+
+it.todo('can handle DockerHub officials (only the package (+ release))');
